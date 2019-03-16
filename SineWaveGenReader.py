2bd482bf62f31192a19d1656671b11ce2192cc4b
Patryk <superpyciek@gmail.com>
2019-03-16 14:37:05 +0100

    Komentarze i drobne poprawki

--- SineWaveGenReader.py
+++ SineWaveGenReader.py
@@ -3,33 +3,36 @@ import struct
 import math
 
 
-def sine_generator(name, freq, duration=10.0, sample_rate=44100.0):
-    values = []
-    wave_file = wave.open(name + ".wav", 'w')
+def sine_generator(freq, duration=10.0, sample_rate=44100.0):
+
+    values = []  # pusta lista na wartości wave'a
+    wave_file = wave.open("sine_" + str(freq) + ".wav", 'w')  # otwarcie wave'a
 
     wave_file.setnchannels(1)  # mono
-    wave_file.setsampwidth(2)
-    wave_file.setframerate(sample_rate)
+    wave_file.setsampwidth(2)  # liczba bitow na których zapisana jest próbka
+    wave_file.setframerate(sample_rate)  # ustawienie f.probkowania
 
-    for i in range(int(duration * sample_rate)):
-        value = int(32767.0 * math.sin(freq * 2.0 * math.pi * float(i) / float(sample_rate)))
-        values.append(value)
-        data = struct.pack('<h', value)
-        wave_file.writeframesraw(data)
+    for i in range(int(duration * sample_rate)):  # iteracja o dlugosci wave'a
+        value = int(32767.0 * math.sin(freq * 2.0 * math.pi * float(i) / float(sample_rate)))  # robienie sinusa
+        values.append(value)  # tworzenie listy z wartosciami do pozniejszego użytku
+        data = struct.pack('<h', value)  # konwersja z int na byte
+        wave_file.writeframesraw(data)  # zapis do wav
 
     wave_file.close()
     return values
 
 
 def wave_to_list(directory):
-    wave_file = wave.open(directory, 'r')
-    wave_length = wave_file.getnframes()
-    list_t = []
-    for i in range(0, wave_length):
-        wave_data = wave_file.readframes(1)
-        data = struct.unpack("<h", wave_data)
-        list_t.append(int(data[0]))
-    return list_t
+    wave_file = wave.open(directory, 'r')  # otwarcie pliku w trybie readonly
+    wave_length = wave_file.getnframes()  # dlugosc pliku
+    values = []  # pusta lista na wartosci wave'a
+
+    for i in range(0, wave_length):  # iteracja po calym pliku
+        wave_data = wave_file.readframes(1)  # zapis do zmiennej typu bytes
+        data = struct.unpack("<h", wave_data)  # zmiana z byte na tuple z intami
+        values.append(int(data[0]))  # konwersja na liste intow
+
+    return values
 
 
-sine_generator("halo_wyspa", 1300.0, 1.0, 44100.0)
+sine_generator(1300.0, 1.0, 44100.0)
--- ZeroCrossingRate.py
+++ ZeroCrossingRate.py
@@ -2,15 +2,14 @@ import SineWaveGenReader
 
 
 def zero_crossing_rate(file_name):
-    prev_value = 0
+    prev_value = 0  # zmienna do przechowywania poprzedniej wartosci
     zero_crossing = 0
-    wave_values = SineWaveGenReader.wave_to_list(file_name)
-    for i in wave_values:
-        value = i
-        if i * prev_value < 0 or value == 0:
+    wave_values = SineWaveGenReader.wave_to_list(file_name)  # lista z wartosciami pliku
+    for value in wave_values:
+        if value * prev_value < 0 or value == 0:  # jesli <= 0 to nastąpiło przejscie przez zero
             zero_crossing += 1
         prev_value = value
     return zero_crossing
 
 
-print(zero_crossing_rate("sinus_440.wav"))
+print(zero_crossing_rate("sine_1300.0.wav"))
