d747a7eb98a0de69c271919fd45d1a6c9f44c11a
Patryk <superpyciek@gmail.com>
2019-03-21 12:49:19 +0100

    pierwsza wersja fft

--- fft.py
+++ fft.py
@@ -1,4 +1,4 @@
-from scipy.fftpack import fft, ifft
+from scipy.fftpack import fft
 import numpy as np
 import wave_to_list_sine_gen
 
@@ -6,7 +6,7 @@ wav_values, wav_values_chunks = wave_to_list_sine_gen.wave_to_list("sine_22000.0
 print(type(wav_values))
 N = len(wav_values)
 print(N)
-T = 1.0/(44100*3)
+T = 1.0/N
 x = np.linspace(0.0, N*T, N)
 y = np.array(wav_values)
 #y = np.sin(50*2.0*np.pi*x)+0.5*np.sin(80.0*2.0*np.pi*x)
