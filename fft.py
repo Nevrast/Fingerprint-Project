7f05ea3d93ee0cc9838320fa2597a97f5f1c0377
Patryk <superpyciek@gmail.com>
2019-03-21 12:27:41 +0100

    pierwsza wersja fft

---
+++ fft.py
@@ -0,0 +1,19 @@
+from scipy.fftpack import fft, ifft
+import numpy as np
+import wave_to_list_sine_gen
+
+wav_values, wav_values_chunks = wave_to_list_sine_gen.wave_to_list("sine_22000.0.wav", False, 44100)
+print(type(wav_values))
+N = len(wav_values)
+print(N)
+T = 1.0/(44100*3)
+x = np.linspace(0.0, N*T, N)
+y = np.array(wav_values)
+#y = np.sin(50*2.0*np.pi*x)+0.5*np.sin(80.0*2.0*np.pi*x)
+print(type(y))
+yf = fft(y)
+xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+import matplotlib.pyplot as plt
+plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+plt.grid()
+plt.show()
--- wave_to_list_sine_gen.py
+++ wave_to_list_sine_gen.py
@@ -1,7 +1,6 @@
 import wave
 import struct
 import math
-import itertools
 
 def sine_generator(freq, duration=10.0, sample_rate=44100.0):
 
@@ -17,7 +16,7 @@ def sine_generator(freq, duration=10.0, sample_rate=44100.0):
         values.append(value)  # tworzenie listy z wartosciami do pozniejszego użytku
         data = struct.pack('<h', value)  # konwersja z int na byte
         wave_file.writeframesraw(data)  # zapis do wav
-
+    print(values)
     wave_file.close()
     return values
 
@@ -29,7 +28,7 @@ def wave_to_list(directory, split_into_chunks = True, number_of_samples = 4410):
     chunk_values = []
     for i in range(0, wave_length):  # iteracja po calym pliku
         wave_data = wave_file.readframes(1)  # zapis do zmiennej typu bytes
-        data = struct.unpack("<hh", wave_data)  # zmiana z byte na tuple z intami
+        data = struct.unpack("<h", wave_data)  # zmiana z byte na tuple z intami
         values.append(int(data[0]))  # konwersja na liste intow
 
     if split_into_chunks:
