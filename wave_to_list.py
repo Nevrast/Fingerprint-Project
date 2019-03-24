91e48642a515a1ab371c0b6898265049253c993a
Patryk <superpyciek@gmail.com>
2019-03-24 09:30:51 +0100

    dostosowana funkcja zcr do fingerprint_creating, zmieniony podzial na chunki TODO: offsetowanie chunkow Dodane zamknięcie pliku po otwarciu, oops

--- wave_to_list.py
+++ wave_to_list.py
@@ -2,26 +2,53 @@ import wave
 import struct
 
 
-def wave_to_list(directory, split_into_chunks=True, number_of_chunks=1):
+def wave_to_list(directory, split_into_chunks=True, window_size=64, offset=0):
 
-    if number_of_chunks < 1:
-        number_of_chunks = 1
+    if window_size < 16:        # minimalne okienko 16 probek
+        window_size = 16
     wave_file = wave.open(directory, 'r')  # otwarcie pliku w trybie readonly
-    sample_freq = wave_file.getframerate()
     wave_length = wave_file.getnframes()  # dlugosc pliku
+
     values = []  # pusta lista na wartosci wave'a
-    chunk_values = []
+    chunk_values = []  # lista na wartości chunków
     for i in range(0, wave_length):  # iteracja po calym pliku
         wave_data = wave_file.readframes(1)  # zapis do zmiennej typu bytes
-        data = struct.unpack("<hh", wave_data)  # zmiana z byte na tuple z intami
+        data = struct.unpack("<h", wave_data)  # zmiana z byte na tuple z intami
         values.append(int(data[0]))  # konwersja na liste intow
-
+    values = normalize(values)
+    print(max(values))
+    # testowe values = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     if split_into_chunks:
-        for i in range(0, len(values), int(sample_freq/number_of_chunks)):
-            chunk = values[i:i + int(sample_freq/number_of_chunks)]
-            chunk_values.append(chunk)
-    """zwraca listę lub number_of_chunks list wartości wave'a"""
+        if offset == 0:
+            for i in range(0, len(values), window_size):  # ciecie listy w chunki o rozmiarze window_size
+                chunk = values[i:i + int(window_size)]
+                chunk_values.append(chunk)
+        else:
+            """
+            TODO: Naprawic, nie działa poprawnie dla wszystkich przypadków
+            """
+            if offset >= 100:
+                offset = 99
+            offset = int(window_size*(100 - offset)/100)
+            for i in range(0, len(values)):
+                if i % offset == 0:
+                    print(i)
+                    chunk = values[i:i + int(window_size)]
+                    chunk_values.append(chunk)
+    wave_file.close()
+
+    """zwraca listę oraz number_of_chunks list wartości wave'a"""
     return values, chunk_values
 
 
-
+def normalize(values):
+    """
+    :param values: lista wartości wave'a
+    :return: znormalizowana lista
+    """
+    maximum = max(values)
+    normalized_values = []
+    for value in values:
+        normalized_value = value/maximum
+        normalized_values.append(normalized_value)
+    return normalized_values
