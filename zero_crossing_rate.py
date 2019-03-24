05bb773ea8a9449d68a11e0f9db07f91d6d72619
Patryk <superpyciek@gmail.com>
2019-03-24 09:30:51 +0100

    dostosowana funkcja zcr do fingerprint_creating, zmieniony podzial na chunki TODO: offsetowanie chunkow Dodane zamknięcie pliku po otwarciu, oops

--- zero_crossing_rate.py
+++ zero_crossing_rate.py
@@ -2,34 +2,36 @@ import wave_to_list
 import numpy as np
 
 
-def zero_crossing_rate(file_name, split_into_chunks):
+def zero_crossing_rate(fingerprint, input_singal, input_chunk=None):
+    """
+    :param fingerprint: list, istniejący fingerprint do ktorego dorzucamy parametry
+    :param input_singal: list, wartości wave'a
+    :param input_chunk: lista2D, wartości wave'a podzielone na sublisty
+    :return: zero crossing rate calego wave'a lub array zcr chunkow
+    """
     prev_value = 0  # zmienna do przechowywania poprzedniej wartosci
     zero_crossing = 0
-    wave_values, wave_chunks = wave_to_list.wave_to_list(file_name, split_into_chunks, 5)  # lista z wartosciami pliku
-    chunk_array = []
     chunk = []
 
-    if not split_into_chunks:
-        for value in wave_values:
+    if input_chunk is None:
+        for value in input_singal:
             if value * prev_value < 0 or value == 0:  # jesli <= 0 to nastąpiło przejscie przez zero
                 zero_crossing += 1
             prev_value = value
-        return zero_crossing  # zwraca zrc w postaci jednego inta
+        fingerprint.append(zero_crossing)
+        return fingerprint
     else:
         zero_crossing_in_chunk = []
-        for i in range(len(wave_chunks)):
+        for i in range(len(input_chunk)):
             chunk.clear()
             zero_crossing = 0
-            for j in wave_chunks[i]:
+            for j in input_chunk[i]:
                 chunk.append(j)
             for value in chunk:
                 if value * prev_value < 0 or value == 0:  # jesli <= 0 to nastąpiło przejscie przez zero
                     zero_crossing += 1
                 prev_value = value
             zero_crossing_in_chunk.append(zero_crossing)
-            chunk_array.append(chunk)
-        return np.array(zero_crossing_in_chunk)  # zwraca numpy.ndarray przejść przez zero
+        fingerprint.append(zero_crossing_in_chunk)
+        return np.array(fingerprint)  # zwraca numpy.ndarray przejść przez zero
 
-
-print(zero_crossing_rate("sine_1300.0.wav", True))
-print(zero_crossing_rate("sine_1300.0.wav", False))
