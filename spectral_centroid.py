6a384571b5eadd671da2c595491c2e65f726d294
Patryk <superpyciek@gmail.com>
2019-03-29 17:18:54 +0100

    pierwsza wersja fft

--- spectral_centroid.py
+++ spectral_centroid.py
@@ -2,20 +2,26 @@ from scipy.fftpack import fft
 import wave_to_list
 import numpy as np
 import matplotlib.pyplot as plt
+import librosa
 
-input_chunk, input_chunk = wave_to_list.wave_to_list("in_the_element.wav", window_size=64)
+input_chunk, input_chunk = wave_to_list.wave_to_list("sine_1000.0.wav", window_size=4096)
 
 
-
-def spectral_centroid(input_chunk, samplerate=44100):
+def spectral_centroid(input_chunks, samplerate=44100):
     param = []
-    for i in range(len(input_chunk)):
-        N = len(input_chunk[i])
-        magnitudes = np.abs(np.fft.rfft(input_chunk[i]))
-        length = len(input_chunk[i])
+    for i in range(len(input_chunks)):
+       # N = len(input_chunks[i])
+        magnitudes = np.abs(np.fft.rfft(input_chunks[i]))
+        length = len(input_chunks[i])
         freqs = np.abs(np.fft.fftfreq(length, 1.0/samplerate)[:length//2+1])
-        param.append((magnitudes*freqs) / np.sum(magnitudes))
+        print(freqs)
+        param.append(np.sum((magnitudes*freqs)) / np.sum(magnitudes))
         #print(freqs)
-    return param #
+    return param
 
+
+for i in range(len(input_chunk)):
+    param = []
+    param.append(librosa.feature.spectral_centroid(np.array(input_chunk[i]), sr=44100))
+   # print(param)
 print(spectral_centroid(input_chunk))
