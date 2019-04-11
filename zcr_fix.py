10f8b7abf541da82ef558789d075e24aabb95027
Patryk <superpyciek@gmail.com>
2019-04-11 22:26:17 +0200

    wave_to_array - added option to fill zeros so last window is not skipped zcr_fix - made working algorithm that returns array of zero crossings per one window

--- zcr_fix.py
+++ zcr_fix.py
@@ -3,44 +3,33 @@ from sine_gen import sine_generator
 import matplotlib.pyplot as plt
 import numpy as np
 
-#sine_generator(100.0, 1.0, 44100, True)
-sr, left, right = wave_to_array('sine_stereo_100.0_44.1kHz.wav', 4096)
-print(sr)
-
-
-
-
-prev_value = 0
-
+#sine_generator(10.0, 1.0, 50, True)
+sr, left, right = wave_to_array('sine_stereo_10.0_0.05kHz.wav', 2**11, fill_zeros=True)
+print(left)
 zcr = np.array([])
+last_value = -1
 for i in left:
-    # i= left[0]
     z_c = 0
     #print(i.shape)
     for j in range(int(i.shape[0])):
         #print(i[j])
-        if i[j-1] == i[-1]:
-            continue
         if np.sign(i[j]) == 0:
             i[j] = -1
+        if j == 0:
+            if np.sign(last_value) != np.sign(i[j]):
+                z_c += 1
+            continue
+
         if np.sign(i[j]) != np.sign(i[j-1]):
             z_c += 1
-
-            #print(i[j], i[j-1])
-    #print(z_c)
+            #print(j)
+    last_value = i[j]
     zcr = np.append(zcr, z_c)
-# print(i[-1])
-# prev_value = i[j]
-# print(i.shape)
-    #zcr.put(crossings_nonzero_all(i))
-    # for j in range(int(i.shape[0])):
-    #     #print(i[j])
-    #     zero_crossings = np.where(np.diff(np.signbit(i[j])))[0]
-    #    # print(i[-1])
-    #     #prev_value = i[j]
+print(zcr[0:])
+print(2**14)
 print(np.sum(zcr, dtype=np.int16))
 #print(k)
-plt.plot(left[2])
+plt.plot(zcr)
 plt.show()
 
 
