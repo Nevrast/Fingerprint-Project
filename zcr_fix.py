0d7bfd59c9790e14b1f026cd55b4dbb055a8d496
Patryk <superpyciek@gmail.com>
2019-04-12 11:13:50 +0200

    defined function WIP

--- zcr_fix.py
+++ zcr_fix.py
@@ -3,33 +3,31 @@ from sine_gen import sine_generator
 import matplotlib.pyplot as plt
 import numpy as np
 
-#sine_generator(10.0, 1.0, 50, True)
-sr, left, right = wave_to_array('sine_stereo_10.0_0.05kHz.wav', 2**11, fill_zeros=True)
-print(left)
-zcr = np.array([])
-last_value = -1
-for i in left:
-    z_c = 0
-    #print(i.shape)
-    for j in range(int(i.shape[0])):
-        #print(i[j])
-        if np.sign(i[j]) == 0:
-            i[j] = -1
-        if j == 0:
-            if np.sign(last_value) != np.sign(i[j]):
-                z_c += 1
-            continue
+sine_generator(10.0, 1.0, 50, True)
+sr, left, right = wave_to_array('yamaha.wav', 1024, fill_zeros=True)
 
-        if np.sign(i[j]) != np.sign(i[j-1]):
-            z_c += 1
-            #print(j)
-    last_value = i[j]
-    zcr = np.append(zcr, z_c)
-print(zcr[0:])
-print(2**14)
-print(np.sum(zcr, dtype=np.int16))
-#print(k)
-plt.plot(zcr)
+
+def zero_crossing(ch_left, ch_right):
+    zcr = np.array([])
+    last_value = -1
+    for i in ch_left:
+        z_c = 0
+        for j in range(int(i.shape[0])):
+            if np.sign(i[j]) == 0:
+                i[j] = -1
+            if j == 0:
+                if np.sign(last_value) != np.sign(i[j]):
+                    z_c += 1
+                continue
+            if np.sign(i[j]) != np.sign(i[j-1]):
+                z_c += 1
+        last_value = i[j]
+        zcr = np.append(zcr, z_c)
+
+
+    print(np.sum(zcr, dtype=np.int16))
+zero_crossing(left, right)
+plt.plot(left.flat)
 plt.show()
 
 
